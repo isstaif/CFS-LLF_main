c545ae90776ec419708b599828baad671fce5647
al-Amjad Isstaif <isstaif@gmail.com>
2025-07-15 10:40:08 +0100

    updating scripts to work with new systemd functions

--- scripts/rd_hashd_benchmark_start.sh
+++ scripts/rd_hashd_benchmark_start.sh
@@ -2,7 +2,7 @@ tail -n +1 ./rd-hashd/params* | grep -v '\/\/'
 
 i=0
 while [ $i -le $1 ]; do
-  sudo systemctl stop func-$i.service
+  sudo systemctl stop func@$i.service
   i=$(( i + 1 ))
 done
 
@@ -11,7 +11,7 @@ rm ./rd-hashd/results/report-*.json
 
 i=0
 while [ $i -le $1 ]; do
-  sudo systemctl start func-$i.service
+  sudo systemctl start func@$i.service
   i=$(( i + 1 ))
 done
 
@@ -19,7 +19,7 @@ sleep 10
 
 i=0
 while [ $i -le $1 ]; do
-  sudo systemctl status func-$i.service
+  sudo systemctl status func@$i.service
   i=$(( i + 1 ))
 done
 
