7e1faba194e0ef871fa66875620edfdd203e74cb
al-Amjad Isstaif <isstaif@gmail.com>
2025-07-15 09:35:18 +0100

    fixing working directory for the benchmark

--- rd-hashd/start-rd-hashd.sh
+++ rd-hashd/start-rd-hashd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /etc/CFS-LLF_env
-cd "$REPO_DIR" || exit 1
-exec /home/aati2/resctl-demo/target/rd-hashd --args "rd-hashd/args/args-$1.json"
+cd "$REPO_DIR/rd-hashd" || exit 1
+exec /home/aati2/resctl-demo/target/rd-hashd --args "args/args-$1.json"
