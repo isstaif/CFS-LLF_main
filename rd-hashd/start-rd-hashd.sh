ba8feb3a2590d5f6809fd946847655be621b5e11
al-Amjad Isstaif <isstaif@gmail.com>
2025-07-15 09:17:13 +0100

    fixing function systemd templates

--- rd-hashd/start-rd-hashd.sh
+++ rd-hashd/start-rd-hashd.sh
@@ -1,5 +1,4 @@
-# /usr/local/bin/start-rd-hashd.sh
 #!/bin/bash
 source /etc/CFS-LLF_env
 cd "$REPO_DIR" || exit 1
-exec /usr/local/bin/rd-hashd --args $1
+exec /home/aati2/resctl-demo/target/rd-hashd --args "rd-hashd/args/args-$1.json"
