fa3e3765363a93a29851c7ef187937026bb995bf
al-Amjad Isstaif <isstaif@gmail.com>
2025-07-15 11:03:06 +0100

    Revert "updating scripts to work with new systemd functions"
    This reverts commit c545ae90776ec419708b599828baad671fce5647.

--- scripts/rd_hashd_benchmark_end.sh
+++ scripts/rd_hashd_benchmark_end.sh
@@ -1,12 +1,12 @@
 i=0
 while [ $i -le $1 ]; do
-  sudo systemctl stop func@$i.service
+  sudo systemctl stop func-$i.service
   i=$(( i + 1 ))
 done
 
 i=0
 while [ $i -le $1 ]; do
-  sudo systemctl status func@$i.service
+  sudo systemctl status func-$i.service
   i=$(( i + 1 ))
 done
 
