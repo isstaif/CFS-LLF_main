7509ab90df6eca81a792608b0b0ab9bf9e1096a4
Amjad Isstaif <aati2@caelum-407.cl.cam.ac.uk>
2026-03-15 09:38:53 +0000

    workload yaml and init script

---
+++ cluster/setup_100pods.sh
@@ -0,0 +1,209 @@
+yq e '.metadata.name = "pytorch-classifier-1"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-2"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-3"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-4"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-5"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-6"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-7"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-8"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-9"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-10"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+
+yq e '.metadata.name = "pytorch-classifier-11"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-12"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-13"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-14"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-15"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-16"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-17"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-18"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-19"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-20"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+
+yq e '.metadata.name = "pytorch-classifier-21"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-22"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-23"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-24"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-25"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-26"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-27"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-28"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-29"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-30"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+
+yq e '.metadata.name = "pytorch-classifier-31"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-32"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-33"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-34"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-35"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-36"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-37"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-38"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-39"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-40"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+
+yq e '.metadata.name = "pytorch-classifier-41"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-42"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-43"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-44"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-45"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-46"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-47"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-48"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-49"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-50"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+
+yq e '.metadata.name = "pytorch-classifier-51"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-52"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-53"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-54"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-55"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-56"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-57"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-58"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-59"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-60"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+
+yq e '.metadata.name = "pytorch-classifier-61"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-62"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-63"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-64"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-65"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-66"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-67"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-68"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-69"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-70"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+
+yq e '.metadata.name = "pytorch-classifier-71"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-72"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-73"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-74"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-75"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-76"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-77"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-78"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-79"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-80"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+
+yq e '.metadata.name = "pytorch-classifier-81"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-82"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-83"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-84"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-85"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-86"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-87"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-88"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-89"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-90"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+
+yq e '.metadata.name = "pytorch-classifier-91"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-92"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-93"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-94"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-95"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-96"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-97"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-98"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-99"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
+yq e '.metadata.name = "pytorch-classifier-100"' pytorch_ksvc.yaml <<EOF | kubectl apply -f - 
+EOF
